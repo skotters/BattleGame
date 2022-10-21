afa1642a04fc8cbe9562705018bb93e324372ae0
Scott <skotters@gmail.com>
2022-10-20 21:58:19 -0400

    first commit for setup

---
+++ BattleGame/BattleGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial commit for github setup");
+        }
+    }
+}
